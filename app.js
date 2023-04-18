f2c1b2e4f954aa2694d1d7fa9d2e78b7c0a59c9f
Anubhav Singh <singhanubhav58@gmail.com>
2023-04-18 14:03:03 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -50,10 +50,9 @@ app.use((req, res, next) => {
   } else {
     res.locals.currentUser = req.session.user_id;
     const userid = req.session.user_id;
-    User.findById(userid, (err, docs) => {
-      res.locals.currentUsername = docs.username;
-      next();
-    });
+    const docs = User.findById(userid);
+    if (docs) res.locals.currentUsername = docs.username;
+    next();
   }
 });
 
@@ -91,28 +90,25 @@ app.get('/login', (req, res) => res.render('login'));
 
 app.get('/failed', (req, res) => res.render('failed'));
 
-app.get('/admin', requireLogin, (req, res) => {
+app.get('/admin', requireLogin, async (req, res) => {
   const uid = req.session.user_id;
-  User.findById(uid, (err, docs) => {
-    if (docs.isAdmin === true) {
-      User.find({}, (err, userdocs) => {
-        res.render('admin', { userdocs });
-      });
-    } else {
-      res.render('message', { msgcode: '7' });
-    }
-  });
+  const docs = await User.findById(uid);
+  if (docs.isAdmin === true) {
+    const userdocs = await User.find({});
+    res.render('admin', { userdocs });
+  } else {
+    res.render('message', { msgcode: '7' });
+  }
 });
 
-app.get('/url', requireLogin, (req, res) => {
+app.get('/url', requireLogin, async (req, res) => {
   const uid = req.session.user_id;
-  User.findById(uid, (err, docs) => {
-    if (docs) {
-      res.render('url', { username: docs.username, formids: docs.formsid });
-    } else {
-      res.render('message', { msgcode: '1' });
-    }
-  });
+  const docs = await User.findById(uid);
+  if (docs) {
+    res.render('url', { username: docs.username, formids: docs.formsid });
+  } else {
+    res.render('message', { msgcode: '1' });
+  }
 });
 
 app.post('/login', async (req, res) => {
@@ -140,7 +136,7 @@ app.post('/contact', (req, res) => {
   res.render('success');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { username, password, name, email, phoneNum } = req.body;
   let isAdmin = false;
   if (username === 'anubhav') {
@@ -148,30 +144,29 @@ app.post('/register', (req, res) => {
   } else {
     isAdmin = false;
   }
-  User.find({ username: username }, async (err, docs) => {
-    if (docs.length) {
-      res.render('message', { msgcode: '3' });
-    } else {
-      const user = new User({
-        username,
-        password,
-        name,
-        email,
-        phoneNum,
-        isAdmin,
-      });
-      await user.save();
-      req.session.user_id = user._id;
-      res.redirect('/');
-    }
-  });
+  const docs = await User.find({ username: username });
+  if (docs.length) {
+    res.render('message', { msgcode: '3' });
+  } else {
+    const user = new User({
+      username,
+      password,
+      name,
+      email,
+      phoneNum,
+      isAdmin,
+    });
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/');
+  }
 });
 
 app.get('/create-forms', requireLogin, (req, res) => {
   res.render('create-forms');
 });
 
-app.post('/create-forms', requireLogin, (req, res) => {
+app.post('/create-forms', requireLogin, async (req, res) => {
   let { formTitle, redirectURL } = req.body;
   if (redirectURL === '') {
     redirectURL = 'https://confused-tan-elk.cyclic.app/success';
@@ -179,134 +174,114 @@ app.post('/create-forms', requireLogin, (req, res) => {
   const shortid = new ShortUniqueId({ length: 10 });
   let uuid = shortid();
   const uid = req.session.user_id;
-  User.findByIdAndUpdate(
-    uid,
-    {
-      $push: {
-        formsid: uuid,
-      },
+  const userdocs = User.findByIdAndUpdate(uid, {
+    $push: {
+      formsid: uuid,
     },
-    (err, userdocs) => {
-      if (err) {
-        console.log(err);
-      } else {
-        Form.create(
-          {
-            formTitle: formTitle,
-            redirectUrl: redirectURL,
-            formid: uuid,
-            username: userdocs.username,
-          },
-          (err, docs) => {
-            if (err) {
-              console.log(err);
-            } else {
-              res.render('url', { docs });
-            }
-          }
-        );
-      }
-    }
-  );
-});
-
-app.get('/form/:user', requireLogin, (req, res) => {
-  const username = req.params.user;
-  User.findOne({ username }, (err, docs) => {
-    if (docs) {
-      Form.find({ username }, (err, docs) => {
-        res.render('user', { docs });
-      });
-    } else {
-      res.render('user', { docs });
-    }
   });
+
+  if (!userdocs) {
+    console.log('Err');
+  } else {
+    const docs = await Form.create({
+      formTitle: formTitle,
+      redirectUrl: redirectURL,
+      formid: uuid,
+      username: userdocs.username,
+    });
+    if (!docs) {
+      console.log(err);
+    } else {
+      res.render('url', { docs });
+    }
+  }
 });
 
-app.post('/form/delete/:fid', (req, res) => {
+app.get('/form/:user', requireLogin, async (req, res) => {
+  const username = req.params.user;
+  const docs = User.findOne({ username });
+  if (docs) {
+    const userdocs = await Form.find({ username });
+    res.render('user', { userdocs });
+  } else {
+    res.render('user', { docs });
+  }
+});
+
+app.post('/form/delete/:fid', async (req, res) => {
   const fid = req.params.fid;
   const uidd = req.session.user_id;
-  Form.findOneAndDelete({ formid: fid }, (err, docs) => {
-    if (docs) {
-      User.findByIdAndUpdate(
-        uidd,
-        {
-          $pull: {
-            formsid: fid,
-          },
-        },
-        (err, userdocs) => {
-          if (userdocs) {
-            res.render('message', { msgcode: '4' });
-          } else {
-            res.render('message', { msgcode: '5' });
-          }
-        }
-      );
+  const docs = await Form.findOneAndDelete({ formid: fid });
+  if (docs) {
+    const userdocs = await User.findByIdAndUpdate(uidd, {
+      $pull: {
+        formsid: fid,
+      },
+    });
+    if (userdocs) {
+      res.render('message', { msgcode: '4' });
     } else {
       res.render('message', { msgcode: '5' });
     }
-  });
+  } else {
+    res.render('message', { msgcode: '5' });
+  }
 });
 
-app.get('/form/:user/:formid', (req, res) => {
+app.get('/form/:user/:formid', async (req, res) => {
   const username = req.params.user;
   const formid = req.params.formid;
-  User.findOne({ username }, (err, docs) => {
-    if (docs) {
-      if (docs.formsid.indexOf(formid) !== -1) {
-        res.json({
-          status: 'success',
-          desciption: 'form is current active and accepting submittions',
-        });
-      } else {
-        res.json({
-          status: 'failed',
-          desciption: 'form is not accepting submittions',
-        });
+  const docs = await User.findOne({ username });
+  if (docs) {
+    if (docs.formsid.indexOf(formid) !== -1) {
+      res.json({
+        status: 'success',
+        desciption: 'form is current active and accepting submittions',
+      });
+    } else {
+      res.json({
+        status: 'failed',
+        desciption: 'form is not accepting submittions',
+      });
+    }
+  } else {
+    res.json({
+      status: 'failed',
+      desciption: 'form is not yet created',
+    });
+  }
+});
+
+app.post('/form/:user/:formid', async (req, res) => {
+  const username = req.params.user;
+  const formid = req.params.formid;
+  const userdocs = await User.findOne({ username });
+  if (userdocs) {
+    if (userdocs.formsid.indexOf(formid) !== -1) {
+      const docs = await Form.findOne({ formid });
+      if (docs) {
+        const data = Object.entries(req.body);
+        let mailData = `Form Submission for ${username} on ${formid}`;
+        mailData += `<br>---------------------------<br>`;
+        for (let i = 0; i < data.length; i++) {
+          mailData += data[i][0].toUpperCase() + ' : ' + data[i][1] + '<br>';
+        }
+        mailData += `---------------------------<br>`;
+        sendMail(mailData, userdocs.email, username);
+        res.redirect(docs.redirectUrl);
       }
     } else {
       res.json({
         status: 'failed',
-        desciption: 'form is not yet created',
+        desciption: 'form is not accepting submittions',
       });
     }
-  });
-});
-
-app.post('/form/:user/:formid', (req, res) => {
-  const username = req.params.user;
-  const formid = req.params.formid;
-  User.findOne({ username }, (err, userdocs) => {
-    if (userdocs) {
-      if (userdocs.formsid.indexOf(formid) !== -1) {
-        Form.findOne({ formid }, (err, docs) => {
-          if (docs) {
-            const data = Object.entries(req.body);
-            let mailData = `Form Submission for ${username} on ${formid}`;
-            mailData += `<br>---------------------------<br>`;
-            for (let i = 0; i < data.length; i++) {
-              mailData +=
-                data[i][0].toUpperCase() + ' : ' + data[i][1] + '<br>';
-            }
-            mailData += `---------------------------<br>`;
-            sendMail(mailData, userdocs.email, username);
-            res.redirect(docs.redirectUrl);
-          }
-        });
-      } else {
-        res.json({
-          status: 'failed',
-          desciption: 'form is not accepting submittions',
-        });
-      }
-    } else {
-      res.json({
-        status: 'failed',
-        desciption: 'form is not yet created',
-      });
-    }
-  });
+  } else {
+    res.json({
+      status: 'failed',
+      desciption: 'form is not yet created',
+    });
+  }
 });
 
 app.get('/logout', (req, res) => {
